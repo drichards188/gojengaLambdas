1d28903fa25b7022ca95fb7d9831c0bacbc2e19e
drichards188 <github@cxoprotect.com>
2022-07-11 13:56:55 -0600

    added sample SNS script that logs messages from an SNS topic

---
+++ python/sns.py
@@ -0,0 +1,10 @@
+import json
+
+print('Loading function')
+
+
+def lambda_handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+    message = event['Records'][0]['Sns']['Message']
+    print("From SNS: " + message)
+    return message
